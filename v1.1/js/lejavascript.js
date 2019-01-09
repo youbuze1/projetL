408cc1e220705f8c5eed696a92ce29f0b9106cf2
skyfolder <marchevillegael@gmail.com>
2019-01-09 11:05:13 +0100

    Add files via upload

--- v1.1/js/lejavascript.js
+++ v1.1/js/lejavascript.js
@@ -1,31 +1,35 @@
-var z;
+var z; //taille d'un carreau
 
 function dessiner(mode){
   var canvas = document.getElementById('fond');
   if (canvas.getContext) {
   var ctx = canvas.getContext('2d');
-  var type;
-  var x = 0;
-  var y =0;
-  var mode;
-  mode=mode;
+  var type; //type de grille (4*4,5*5 ou 6*6)
+  var x = 300; //position x de la grille
+  var xmax = x+600; //fin de la grille
+  var y =0; //position y de la grille 
   type = Math.floor(Math.random() * Math.floor(3));
 
 switch (type){
 	
 case 0:
+//la grille fait ici 4*4
 
 z = (1/4)*600;
 
-while ( x < 600 ){
-	
+// z vaut 1/4 de la taille de la grille
+
+while ( x < xmax ){
+
 while (y < 600){
 ctx.fillRect(x, y, z, z);
 ctx.clearRect(x+1.5, y+1.5, z-3, z-3);
 y = y+z;	
+//temps que y est plus petit que 600, on dessine un carreau en dessous
 }
 y = 0;
 x = x + z;
+//temps que x est plus petit que xmax, on dessine un carreau à droite
 }
 
 break;
@@ -34,7 +38,7 @@ case 1:
 
 z = (1/5)*600;
 
-while ( x < 600 ){
+while ( x < xmax ){
 
 while (y < 600){
 ctx.fillRect(x, y, z, z);
@@ -50,7 +54,7 @@ case 2:
 
 z = (1/6)*600;
 
-while ( x < 600){
+while ( x < xmax){
 
 while (y < 600){
 ctx.fillRect(x, y, z, z);
@@ -67,21 +71,21 @@ break;
 }
 }
 
-function dessiner_piece(forme,mode){
+function dessiner_piece(forme,angle,xdep){
 var forme;
-var angle=Math.floor(Math.random()*4);
-var xdep=0; //position x départ pour translate
-var ydep=0; //position y départ pour translate
+var angle;
+var xdep; //position x départ pour translate
+var ydep=z; //position y départ pour translate
 var rotate=0; //rotate est l'angle d'inclinaison de la piece
-mode=mode;
+
 
 if(angle==0){
-	var xdep=0; 
-	var ydep=0;
+	/*var xdep=0; 
+	var ydep=0;*/
 	var rotate=0; 
 }
 if(angle==1){ //change la position de xdep et ou ydep pour gérer l'angle de la pièce
-	xdep=3*z;
+	/*xdep=3*z;*/
 	rotate=Math.PI/2; //piece à 90°
 }
 if(angle==2){
@@ -97,7 +101,7 @@ if(angle==3){
 switch(forme){
 case 0:
 		ctx.beginPath();
-			ctx.translate(xdep,ydep);
+			ctx.translate(0,ydep);
 			ctx.rotate(rotate);
 			ctx.fillStyle = "rgba(0,0,0,1)";
 			ctx.fillRect(0,0,2*z,z);
@@ -107,7 +111,7 @@ case 0:
 break;
 case 1:
 	ctx.beginPath();
-		ctx.translate(xdep,ydep);
+		ctx.translate(1200,ydep);
 		ctx.rotate(rotate);
 		ctx.fillStyle = "rgba(0,0,0,1)";
 		ctx.fillRect(0,0,2*z,z);
@@ -119,5 +123,6 @@ default:
 break;
 }
 }
-dessiner_piece(Math.floor(Math.random()*2));
+dessiner_piece(0,0);
+dessiner_piece(1,2);
 }
